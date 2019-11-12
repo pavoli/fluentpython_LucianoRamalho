fbdfcfe1303397c0b25f63075301e5f31a8c7472
polly <pavel.olifer@gmail.com>
2019-11-12 21:18:24 +0300

    add vector class

--- chapter_1/2_vector.py
+++ chapter_1/2_vector.py
@@ -1,10 +1,33 @@
 # -*- coding: utf-8 -*-
 __author__ = 'p.olifer'
 
-
-def main():
-    pass
+from math import hypot
 
 
-if __name__ == '__main__':
-    pass
+class Vector():
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Vector(%r, %r)' % (self.x, self.y)
+
+    def __abs__(self):
+        return hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, other):
+        return Vector(self.x * other, self.y * other)
+
+v = Vector(1, 4)
+v2 = Vector(2, 3)
+print(v + v2)
+print(v2 * 3)
