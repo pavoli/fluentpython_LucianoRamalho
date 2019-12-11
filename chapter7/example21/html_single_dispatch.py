1d1a46807ca3e9ac09b8b8891e67d1d4be9115c6
polly <pavel.olifer@gmail.com>
2019-12-11 06:53:36 +0300

    single dispatch

--- chapter7/example21/html_single_dispatch.py
+++ chapter7/example21/html_single_dispatch.py
@@ -7,9 +7,32 @@ import numbers
 import html
 
 
-def main():
-    pass
+@singledispatch
+def htmlize(obj):
+    content = html.escape(repr(obj))
+    return '<pre>{}</pre>'.format(content)
+
+
+@htmlize.register(str)
+def _(text):
+    content = html.escape(text).replace('\n', '<br>\n')
+    return '<p>{0}</p>'.format(content)
+
+
+@htmlize.register(numbers.Integral)
+def _(n):
+    return '<pre>{0} (0x{0:x})</pre>'.format(n)
+
+
+@htmlize.register(tuple)
+@htmlize.register(abc.MutableSequence)
+def _(seq):
+    inner = '</li>\n<li>'.join(htmlize(item) for item in seq)
+    return '<ul>\n<li>' + inner + '</li>\n</ul>'
 
 
 if __name__ == '__main__':
-    pass
+    print(htmlize({1, 2, 3}))
+    print(htmlize(abs))
+    print(htmlize(42))
+    print(htmlize(['alpha', 33, {3, 2, 1}]))
