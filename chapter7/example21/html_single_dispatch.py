6faebc5f7bb4fa256b93fe58970638c13427f9f0
polly <pavel.olifer@gmail.com>
2019-12-11 06:51:59 +0300

    single dispatch

---
+++ chapter7/example21/html_single_dispatch.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+__author__ = 'p.olifer'
+
+from functools import singledispatch
+from collections import abc
+import numbers
+import html
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    pass
