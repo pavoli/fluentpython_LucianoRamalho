87be1b5ff57703491b937f0e539f7e9eeb6952ea
p.olifer <pavel.olifer@gmail.com>
2019-12-17 17:31:00 +0300

    add hash

--- chapter9/example9/vector2d_v0.py
+++ chapter9/example9/vector2d_v0.py
@@ -11,8 +11,16 @@ class Vector2d:
     typecode = 'd'
 
     def __init__(self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+        self.__x = float(x)
+        self.__y = float(y)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
 
     def __iter__(self):
         return (i for i in (self.x, self.y))
@@ -31,12 +39,43 @@ class Vector2d:
     def __eq__(self, other):
         return tuple(self) == tuple(other)
 
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
+
     def __abs__(self):
         return math.hypot(self.x, self.y)
 
     def __bool__(self):
         return bool(abs(self))
 
-    def __format__(self, format_spec=''):
-        components = (format(c, format_spec) for c in self)
-        return '({}, {})'.format(*components)
+    def __format__(self, fmt_spec=''):
+        if fmt_spec.endswith('p'):
+            fmt_spec = fmt_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_fmt = '<{}, {}>'
+        else:
+            coords = self
+            outer_fmt = '({}, {})'
+        components = (format(c, fmt_spec) for c in coords)
+        return outer_fmt.format(*components)
+
+    def angle(self):
+        return math.atan2(self.y, self.x)
+
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(*memv)
+
+if __name__ == '__main__':
+    #print(format(Vector2d(1, 1), 'p'))
+    #print(format(Vector2d(1, 1), '.3ep'))
+    #print(format(Vector2d(1, 1), '.5fp'))
+    v1 = Vector2d(3, 4)
+    v2 = Vector2d(3.1, 4.2)
+    # print(hash(v1))
+    # print(hash(v2))
+    # print(set([v1, v2]))
+    # print(len(set([v1, v2])))
+    print(v1.__dict__)
