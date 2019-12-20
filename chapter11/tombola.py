b4ee697f6e060590049972dfc6362e8fbaaeb089
p.olifer <pavel.olifer@gmail.com>
2019-12-20 22:48:38 +0300

    leanrn an abstract classes

---
+++ chapter11/tombola.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'p.olifer'
+__version__ = '1.0'
+
+import  abc
+
+class Tonbola(abc.ABC):
+
+    @abc.abstractmethod
+    def load(self, iterable):
+        """add elements from itarebla object"""
+
+    @abc.abstractmethod
+    def pick(self):
+        """
+        function return a random element
+        this method should raise an exception 'LookupError'
+        if object is empty
+        """
+
+    def loaded(self):
+        """
+        return 'True' if exists only 1 element, else return 'False'
+        """
+        return bool(self.inspect())
+
+    def inspect(self):
+        """
+        return sorted tuple
+        """
+        items = []
+        while True:
+            try:
+                items.append(self.pick())
+            except LookupError:
+                break
+        self.load(items)
+        return tuple(sorted(items))
+
+class Fake(Tonbola):
+    def pick(self):
+        return 13
+
+
+if __name__ == '__main__':
+    f = Fake()
