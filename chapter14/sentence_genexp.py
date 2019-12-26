732a8d6269b2bd22f2f9fa0be39200549617940a
p.olifer <pavel.olifer@gmail.com>
2019-12-26 07:49:17 +0300

    generator expression

---
+++ chapter14/sentence_genexp.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'p.olifer'
+__version__ = '1.0'
+
+import re
+import reprlib
+
+RE_WORD = re.compile('\w+')
+
+class Sentence:
+
+    def __init__(self, text):
+        self.text = text
+        self.words = RE_WORD.findall(text)
+
+    def __repr__(self):
+        return 'Sentence(%s) ' % reprlib.repr(self.text)
+
+    def __iter__(self):
+        return (match.group() for match in RE_WORD.finditer(self.text))
+
+
+if __name__ == '__main__':
+    pass
